b5fcb2bea4062a1dcb086c7bef0cd046b5b988cf
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-11-07 15:42:12 -0700

    takes n and r1, r2 .. rn as command line args

--- main.cpp
+++ main.cpp
@@ -3,14 +3,23 @@
 int main(int argc, char** argv) {
 
 	if (argc < 3) {
-		std::cout << "usage: CnKr <cycle length> <complete graph size> \n\n";
+		std::cout << "usage: CnKr <cycle length> r1 r2 .. rn \n\n";
 		return 1;
 	}
 
-	int n = atoi(argv[1]);
-	int r = atoi(argv[2]);
+	unsigned vertices = argc - 2;
+	int r [vertices];
 
-	std::cout << "n is " << n << ", r is " << r << std::endl;
+	for (int i = 0; i < vertices; ++i) {
+		r[i] = atoi(argv[i+2]);
+	}
+
+	int n = atoi(argv[1]);
+
+	std::cout << "n is " << n << std::endl;
+	for (int i = 0; i < vertices; ++i) {
+		std::cout << "r[" << i << "] is " << r[i] << std::endl;
+	}
 
 	return 0;
 }
