7511bee4f483d043dacb676850e4576959dd5d3a
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-11-11 10:52:01 -0700

    constructor was inline defined in header - fixed it.

--- rjf_graphThickness.cpp
+++ rjf_graphThickness.cpp
@@ -1,6 +1,49 @@
 #include "rjf_graphThickness.h"
 #include <iostream>
 
+thickness::thickness(int inputN, int* inputRArray, int inputRSize): n(inputN), r(inputRArray), rSize(inputRSize) {
+	// find # of vertices in cycle, multiply against # of vertices in subgraphs
+	// or add # of vertices in subgraphs together if defined.
+	
+	matrixSize = 0;
+	if (checkIfRValuesAreEqual (inputRArray, inputRSize)) {
+		// just multiply r[0] * n
+		matrixSize = n * r[0];
+		std::cout << "matrixSize (n * r[0]) is " << matrixSize << "\n";
+	} else {
+		// add all values in r[]
+		for (int i = 0; i < rSize; ++i) {
+			matrixSize += inputRArray[i];
+		}
+		std::cout << "matrixSize (sum) is " << matrixSize << "\n";
+	}
+
+	// this may not be used, but, it's the largest subgraph size
+	// could be helpful if we set up the adj. matrix to be of length
+	// n * subGraphSize, so there is a consistent step size between
+	// subgraphs on the matrix. but, then there would be the cases of
+	// 'this vertex intentionally left blank' which seems dubious.
+	//subGraphSize = findLargestR(inputRArray, inputRSize);
+	//std::cout << "subGraphSize is " << subGraphSize << "\n";
+
+
+	// instantiate adjacency matrix and vertex coloring array
+	// and initialize everything EVERRRRYTHING to zero
+	adjacencyMatrix = new int*[matrixSize];
+	for(int i = 0; i < matrixSize; ++i) {
+		adjacencyMatrix[i] = new int[matrixSize];
+		for (int j = 0; j < matrixSize; ++j) {
+			adjacencyMatrix[i][j] = 0;
+		}
+	}
+	vertexColor = new int[matrixSize];
+	for (int i = 0; i < matrixSize; ++i) {
+		vertexColor[i] = 0;
+	}
+
+	populateMatrix();
+}
+
 int thickness::stepByCycleVertex(int cycleVertex) {
 	int step = 0;
 	for (int i = 0; i < cycleVertex; ++i) {
