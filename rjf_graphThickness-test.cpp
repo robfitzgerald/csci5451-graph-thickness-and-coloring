7c690d2f7f2eb81ed028c8142217a19743513393
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-11-10 20:22:46 -0700

    begun thickness class. takes the same inputs as chromaticNumber(). the constructor sets up a 2D array and a 1D array of the length of # of vertices in the graph. the 2D array is an adjacency matrix, and the 1D array will keep track of vertex coloring. console stubs are in to show correct calculation.

---
+++ rjf_graphThickness-test.cpp
@@ -0,0 +1,9 @@
+#include "rjf_graphThickness.h"
+
+int main () {
+
+	int subGraphs [5] = {4,4,6,4,3};
+	thickness(5, subGraphs, 5);
+
+	return 0;
+}
--- rjf_graphThickness.h
+++ rjf_graphThickness.h
@@ -1,6 +1,55 @@
 #ifndef graphThickness_h
 #define graphThickness_h
+#include "chromaticNumber.h"
+#include <iostream>
 
 
+class thickness {
+public:
+	thickness(int inputN, int* inputRArray, int inputRSize): n(inputN), r(inputRArray), rSize(inputRSize)
+	{
+		// find # of vertices in cycle, multiply against # of vertices in subgraphs
+		// or add # of vertices in subgraphs together if defined.
+		
+		int arraySize = 0;
+		if (checkIfRValuesAreEqual (inputRArray, inputRSize)) {
+			// just multiply r[0] * n
+			arraySize = n * r[0];
+			std::cout << "arraySize (n * r[0]) is " << arraySize << "\n";
+		} else {
+			// add all values in r[]
+			for (int i = 0; i < rSize; ++i) {
+				arraySize += inputRArray[i];
+			}
+			std::cout << "arraySize (sum) is " << arraySize << "\n";
+		}
+
+		subGraphSize = findLargestR(inputRArray, inputRSize);
+		std::cout << "subGraphSize is " << subGraphSize << "\n";
+
+		// instantiate adjacency matrix and vertex coloring array
+		// and initialize everything EVERRRRYTHING to zero
+		adjacencyMatrix = new int*[arraySize];
+		for(int i = 0; i < arraySize; ++i) {
+			adjacencyMatrix[i] = new int[arraySize];
+			for (int j = 0; j < arraySize; ++j) {
+				adjacencyMatrix[i][j] = 0;
+			}
+		}
+		vertexColor = new int[arraySize];
+		for (int i = 0; i < arraySize; ++i) {
+			vertexColor[i] = 0;
+		}
+
+	}
+
+private:
+	int n;
+	int subGraphSize;
+	int* r;
+	int** adjacencyMatrix;
+	int* vertexColor;
+	int rSize;
+};
 
 #endif
